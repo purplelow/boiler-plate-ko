49aa5c623886333bc22fc531eb645d489d318b3c
Js-iMac27 <purplelow1@gmail.com>
2021-03-02 01:24:56 +0900

    iMac Redux~Hooks

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,34 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//import * as serviceWorker from './serviceWorker';
+
+import { Provider } from 'react-redux';
+
+import 'antd/dist/antd.css';
+
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers'
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+  <Provider
+    store={createStoreWithMiddleware(Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__&&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+  >
+
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+  , document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
