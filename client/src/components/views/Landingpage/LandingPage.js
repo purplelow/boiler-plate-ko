0087f005288953ff494d839cb758c19b8df8cabf
Js-iMac27 <purplelow1@gmail.com>
2021-03-08 01:52:10 +0900

    iMac LoginPage[redux]

--- client/src/components/views/Landingpage/LandingPage.js
+++ client/src/components/views/Landingpage/LandingPage.js
@@ -9,8 +9,10 @@ function LandingPage() {
     }, [])
 
     return (
-        <div>
-            LandingPage 랜딩페이지
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center', width:'100%', height: '100vh'
+        }}>
+            <h2>LandingPage</h2>
         </div>
     )
 }
